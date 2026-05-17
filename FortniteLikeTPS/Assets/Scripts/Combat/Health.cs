2d417fa1775e71a072cb57430e6149d3574b99c9
ynoda687 <y.noda.687@gmail.com>
2026-05-17 15:13:26 +0900

    Update Health.cs

--- FortniteLikeTPS/Assets/Scripts/Combat/Health.cs
+++ FortniteLikeTPS/Assets/Scripts/Combat/Health.cs
@@ -1,1 +1,28 @@
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    public int maxHealth = 100;
+    int currentHealth;
+
+    void Awake()
+    {
+        currentHealth = maxHealth;
+    }
+
+    public void TakeDamage(int amount)
+    {
+        currentHealth -= amount;
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        Debug.Log($"{gameObject.name} died");
+        // リスポーンやリセットは GameManager 側でやる想定でもOK
+    }
+}
 
