8457cf6bc2776d0382ea21af652598d9700bb785
ynoda687 <y.noda.687@gmail.com>
2026-05-17 15:14:04 +0900

    Update CrosshairController.cs

--- FortniteLikeTPS/Assets/Scripts/UI/CrosshairController.cs
+++ FortniteLikeTPS/Assets/Scripts/UI/CrosshairController.cs
@@ -1,1 +1,19 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CrosshairController : MonoBehaviour
+{
+    public Image crosshairImage;
+
+    void Start()
+    {
+        if (crosshairImage != null)
+        {
+            // Canvas が Screen Space Overlay の前提
+            crosshairImage.rectTransform.anchorMin = new Vector2(0.5f, 0.5f);
+            crosshairImage.rectTransform.anchorMax = new Vector2(0.5f, 0.5f);
+            crosshairImage.rectTransform.anchoredPosition = Vector2.zero;
+        }
+    }
+}
 
