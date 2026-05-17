dbbd159f37cb2aa446f79e4b4a566dc4456996ff
ynoda687 <y.noda.687@gmail.com>
2026-05-17 15:13:16 +0900

    Update Bullet.cs

--- FortniteLikeTPS/Assets/Scripts/Combat/Bullet.cs
+++ FortniteLikeTPS/Assets/Scripts/Combat/Bullet.cs
@@ -1,1 +1,23 @@
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float lifeTime = 3f;
+    public int damage = 20;
+
+    void Start()
+    {
+        Destroy(gameObject, lifeTime);
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        Health hp = other.gameObject.GetComponent<Health>();
+        if (hp != null)
+        {
+            hp.TakeDamage(damage);
+        }
+        Destroy(gameObject);
+    }
+}
 
